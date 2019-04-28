2a11ad378b72d7f680633bc72d3a6ecaeb92c9e1
Ngichu <brianngichu@gmail.com>
2019-04-29 01:05:16 +0300

    add arrays

--- scripts.js
+++ scripts.js
@@ -2,4 +2,5 @@ function triangle() {
     var side1 = parseInt(document.getElementById("side1").value);
     var side2 = parseInt(document.getElementById("side2").value);
     var base = parseInt(document.getElementById("base").value);
+    var sides = [side1, side2, base];
 }
