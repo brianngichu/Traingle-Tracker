9f6da34554120275d07799ec64b4b0fa0de9729c
Ngichu <brianngichu@gmail.com>
2019-04-29 01:04:25 +0300

    add variable base

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 function triangle() {
     var side1 = parseInt(document.getElementById("side1").value);
     var side2 = parseInt(document.getElementById("side2").value);
+    var base = parseInt(document.getElementById("base").value);
 }
