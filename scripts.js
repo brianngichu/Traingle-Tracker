a49c24060d4662709d220c700db60c9da3328ee9
Ngichu <brianngichu@gmail.com>
2019-04-29 01:08:06 +0300

    add if condition for positive intergers

--- scripts.js
+++ scripts.js
@@ -4,4 +4,7 @@ function triangle() {
     var base = parseInt(document.getElementById("base").value);
     var sides = [side1, side2, base];
     var answer;
+    if (side1 <= 0 || side2 <=0 || base <=0){
+        answer = "use positive intergers";
+    }
 }
